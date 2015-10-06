e3a80dbe026325d61d2fa7141e634e24556236dc
mshr <neeerveless@gmail.com>
2015-10-06 13:08:22 +0900

    説明用に変更

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,19 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    wait: {
-      options: {
-        delay: 1500
-      }
+    shell: {
+        sleep: {
+            command: 'sleep 2s'              // サーバが再起動するまで待つ
+        }
     },
     waitServer: {
       server: {
         options: {
-          req: 'http://localhost:3004',
+          req: 'your sever URL',             // 監視するサーバのURL
           fail: function () {
-            console.error('the server had not start'); 
+            console.error('the server had not start');
           },
-          timeout: 20 * 1000,
+          timeout: 50 * 1000,                // ここは微調整必要かも
           isforce: true,
           interval: 200,
           print: true
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
     },
     watch: {
       reload: {
-        files: ['root/**/*', 'lib/**/*'],
-        tasks: ['wait:', 'waitServer:'],
+        files: ['root/**/*', 'lib/**/*'],    // 監視対象ファイル
+        tasks: ['shell:sleep', 'waitServer:'],
         options: {
           livereload: true,
         },
@@ -34,14 +34,14 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');        // 監視
-  grunt.loadNpmTasks('grunt-notify');               // GrowlNotify
-  grunt.loadNpmTasks('grunt-wait');          // server起動確認
-  grunt.loadNpmTasks('grunt-wait-server');          // server起動確認
+  grunt.loadNpmTasks('grunt-contrib-watch');  // 監視
+  grunt.loadNpmTasks('grunt-notify');         // GrowlNotify
+  grunt.loadNpmTasks('grunt-shell');          // severの再起動が始まるのを待つ
+  grunt.loadNpmTasks('grunt-wait-server');    // server起動確認
 
   // Default task.
   grunt.registerTask('default', 'watch');
-  grunt.registerTask('manual', ['wait', 'waitServer']);
+  grunt.registerTask('manual', ['shell', 'waitServer']);
 
   // GrowlNotify
   grunt.task.run('notify_hooks');
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-これでCatalystでもLiveReloadできる？
+# CatalystでLiveReloadをしよう
+
+Node.jsのgruntを使ってファイルの更新を監視、ブラウザのリロードを行う。  
+前提としてNode.jsが必要。導入は[こちら](https://github.com/neeerveless/node)こちらを参照。
+
+## grunt-cliをインストール
+gruntを実行するために必要なのでインストールする。
+
+```:command
+$ npm install -g grunt-cli
+```
+
+## LiveReloadの準備
+### 監視プロセスの準備
+
+```:command
+$ cp Grungfile.js path_to_catalyst_application/
+$ cp package.json path_to_catalyst_application/
+$ cd path_to_catalyst_application && npm install
+```
+※Grungfile.jsは必要に応じて変更しよう。コメント参照。  
+
+### ブラウザプラグインの準備
+[こちら](http://livereload.com/extensions/)から自分の使っているブラウザのプラグインをインストール。  
+FireFoxのプラグインでは設定がなかったのでLiveReloadの接続先がlocalhost:35729になってた。  
+VirtualBoxで開発している場合はポートフォワーディングの設定が必要。  
+![ポートフォワーディング](/images/port_forward.png)
+
+## LiveReloadのスタート
+1.Catalystサーバを起動する  
+2.監視プロセス起動  
+3.ブラウザでサーバに接続する  
+4.ブラウザからLiveReloadに接続する  
+
+### 2.監視プロセス起動
+
+```:command
+$ cd path_to_catalyst_application
+$ grunt
+```
+
+### 4.ブラウザからLiveReloadに接続する
+↓にあるブラウザのアイコンを押してLiveReloadに接続する。失敗のダイアログがでてもくじけず連打ｗ  
+![無効](/images/before.png)  
+↓になったら有効になってる  
+![有効](/images/after.png)  
+
+後は実際にソースコードを編集してみよう！
+
+
+
